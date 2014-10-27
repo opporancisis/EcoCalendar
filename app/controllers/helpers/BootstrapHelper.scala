1f99aa238acc9853cc53089f359419576357ecfc
opporancisis <egorov.aleksey@gmail.com>
2014-10-28 02:16:48 +0300

    form fields handling code more enhancements

--- app/controllers/helpers/BootstrapHelper.scala
+++ app/controllers/helpers/BootstrapHelper.scala
@@ -3,5 +3,5 @@ package controllers.helpers
 import views.html.helper.FieldConstructor
 
 object BootstrapHelper {
-  implicit val myFields = FieldConstructor(views.html.bootstrap.input.f)
+  implicit val myFields = FieldConstructor(views.html.bootstrap.field.f)
 }
